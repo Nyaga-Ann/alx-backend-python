90e1f3bb567da00ffc212eb3e60e018916281f23
Ann Nyaga <anngakii02@gmail.com>
2025-05-30 13:35:56 +0300

    ipdate

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -67,6 +67,6 @@ class TestMemoize(unittest.TestCase):
         with patch.object(TestClass, 'a_method') as mock_method:
             mock_method.return_value = 42
             obj = TestClass()
-            self.assertEqual(obj.a_property(), 42)
-            self.assertEqual(obj.a_property(), 42)
+            self.assertEqual(obj.a_property, 42)
+            self.assertEqual(obj.a_property, 42)
             mock_method.assert_called_once()
