9e5f521ca4585d4fd3423ae3b17979d9422327bf
Ann Nyaga <anngakii02@gmail.com>
2025-05-30 11:42:45 +0300

    Fix SyntaxError and Pycodestyle issues

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -66,5 +66,8 @@ class TestMemoize(unittest.TestCase):
 
         with patch.object(TestClass, 'a_method') as mock_method:
             mock_method.return_value = 42
-            obj =
+            obj = TestClass()
+            self.assertEqual(obj.a_property(), 42)
+            self.assertEqual(obj.a_property(), 42)
+            mock_method.assert_called_once()
 
