5681cea2af0cfd32a70539667300aa5e9188b214
Ann Nyaga <anngakii02@gmail.com>
2025-06-02 16:44:20 +0300

    Update

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -76,7 +76,21 @@ class TestGithubOrgClient(unittest.TestCase):
     "expected_repos",
     "apache2_repos"
 ), [
-    # Leave this blank – ALX will inject these fixtures
+    (
+        {
+            "login": "google",
+            "id": 1,
+            "url": "https://api.github.com/orgs/google",
+            "repos_url": "https://api.github.com/orgs/google/repos"
+        },
+        [
+            {"name": "repo1", "license": {"key": "apache-2.0"}},
+            {"name": "repo2", "license": {"key": "mit"}},
+            {"name": "repo3", "license": {"key": "apache-2.0"}},
+        ],
+        ["repo1", "repo2", "repo3"],
+        ["repo1", "repo3"]
+    )
 ])
 class TestIntegrationGithubOrgClient(unittest.TestCase):
     """Integration tests for GithubOrgClient.public_repos"""
