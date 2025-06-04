ac8c5634a0e7b00d08e22ca02b67d37d78ee754f
Ann Nyaga <anngakii02@gmail.com>
2025-06-04 23:21:24 +0300

    update

--- messaging_app/messaging_app/urls.py
+++ messaging_app/messaging_app/urls.py
@@ -4,5 +4,6 @@ from django.urls import path, include  # include added
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('chats.urls')),  # include the chats app URLs here
+    path('api-auth/', include('rest_framework.urls')),
 ]
 
