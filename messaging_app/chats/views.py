e20e8ec3e60a26187d02dac31c88ad01999fa4d4
Ann Nyaga <anngakii02@gmail.com>
2025-06-08 19:01:27 +0300

    update

--- messaging_app/chats/views.py
+++ messaging_app/chats/views.py
@@ -7,7 +7,10 @@ from rest_framework.exceptions import PermissionDenied
 from .models import Conversation, Message, User
 from .serializers import ConversationSerializer, MessageSerializer, UserSerializer
 from .permissions import IsParticipantOfConversation  # ✅ Import custom permission
-
+from rest_framework import filters
+from django_filters.rest_framework import DjangoFilterBackend
+from .pagination import MessagePagination
+from .filters import MessageFilter
 
 class ConversationViewSet(viewsets.ModelViewSet):
     queryset = Conversation.objects.all()
@@ -37,28 +40,27 @@ class ConversationViewSet(viewsets.ModelViewSet):
             return Response({'error': 'User not found'}, status=status.HTTP_404_NOT_FOUND)
 
 
-class MessageViewSet(ModelViewSet):
+class MessageViewSet(viewsets.ModelViewSet):
     queryset = Message.objects.all()
     serializer_class = MessageSerializer
     permission_classes = [IsAuthenticated, IsParticipantOfConversation]
-    filter_backends = [filters.OrderingFilter]
+    filter_backends = [DjangoFilterBackend, filters.OrderingFilter]
+    filterset_class = MessageFilter  # ✅
     ordering_fields = ['timestamp']
     ordering = ['-timestamp']
+    pagination_class = MessagePagination  # ✅
 
     def get_queryset(self):
         return Message.objects.filter(conversation__participants=self.request.user)
 
     def perform_create(self, serializer):
-        conversation_id = self.request.data.get('conversation_id')  # ✅ Required for the check
+        conversation_id = self.request.data.get('conversation_id')
         try:
             conversation = Conversation.objects.get(id=conversation_id)
         except Conversation.DoesNotExist:
             return Response({'detail': 'Conversation not found.'}, status=status.HTTP_404_NOT_FOUND)
 
         if self.request.user not in conversation.participants.all():
-            return Response(
-                {'detail': 'You are not a participant in this conversation.'},
-                status=status.HTTP_403_FORBIDDEN  # ✅ Required for the check
-            )
+            return Response({'detail': 'You are not a participant in this conversation.'}, status=status.HTTP_403_FORBIDDEN)
 
         serializer.save(sender=self.request.user, conversation=conversation)
