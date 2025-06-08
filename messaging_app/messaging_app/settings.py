144dc22075b196d64c1a7b2713bbf920a5f8a767
Ann Nyaga <anngakii02@gmail.com>
2025-06-08 16:42:50 +0300

    update

--- messaging_app/messaging_app/settings.py
+++ messaging_app/messaging_app/settings.py
@@ -129,9 +129,11 @@ AUTH_USER_MODEL = 'chats.User'
 # ✅ REST Framework settings
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework_simplejwt.authentication.JWTAuthentication',
+        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
     ],
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticated',
     ],
 }
+
