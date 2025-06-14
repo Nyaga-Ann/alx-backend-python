23e779889e68aa4630f5b1789a1c48fcfe113a0c
Ann Nyaga <anngakii02@gmail.com>
2025-06-14 22:33:11 +0300

    update

--- Django-signals_orm-0x04/messaging/signals.py
+++ Django-signals_orm-0x04/messaging/signals.py
@@ -1,4 +1,4 @@
-from django.db.models.signals import post_save, pre_save
+from django.db.models.signals import post_save, pre_save, post_delete
 from django.dispatch import receiver
 from .models import Message, Notification, MessageHistory
 
--- Django-signals_orm-0x04/messaging/views.py
+++ Django-signals_orm-0x04/messaging/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
+from django.shortcuts import get_object_or_404, redirect
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+@login_required
+def delete_user(request):
+    user = request.user
+    user.delete()
+    # Redirect to a goodbye page or home page
+    return redirect('home')  
